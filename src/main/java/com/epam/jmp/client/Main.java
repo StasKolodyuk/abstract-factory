a28488fd21099335e38b06c704a1c7dc372f2d3c
Kaladziuk, Stanislau <stanislau.kaladziuk@ihg.com>
2015-12-14 16:03:34 +0300

    Pass DB/File Choice through Command Line Arguments

--- src/main/java/com/epam/jmp/client/Main.java
+++ src/main/java/com/epam/jmp/client/Main.java
@@ -10,18 +10,25 @@ import com.epam.jmp.api.Person;
 
 public class Main {
 
+    private static final String DB_URL = "jdbc:h2:./test;INIT=RUNSCRIPT FROM 'classpath:create.sql'";
+    private static final String DB_LOGIN = "user";
+    private static final String DB_PASSWORD = "";
+
+    private static final String FILE_NAME = "persons.txt";
+
     public static void main(String[] args) throws ResourceException{
+
+        PersonManager personManager;
+
+        if(args.length == 1 && "db".equals(args[0])) {
+            personManager = PersonManager.from(DB_URL, DB_LOGIN, DB_PASSWORD);
+        } else {
+            //Use file by default
+            personManager = PersonManager.from(FILE_NAME);
+        }
 
         Person mentee = new Person("Stanislau", "Kaladziuk");
         Person mentor = new Person("Viktar", "Strok");
-
-        /* Uncomment to use H2 in-memory database
-        String url = "jdbc:h2:./test;INIT=RUNSCRIPT FROM 'classpath:create.sql'";
-        String login = "user";
-        String password = "";
-        PersonManager personManager = PersonManager.from(url, login, password);*/
-
-        PersonManager personManager = PersonManager.from("persons.txt");
 
         PersonWriter personWriter = personManager.getWriter();
         PersonReader personReader = personManager.getReader();
