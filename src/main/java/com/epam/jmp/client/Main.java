6f6409602f28a13a7a6248603fec115e010c67c4
Stas <staskolodyuk@gmail.com>
2015-12-21 01:21:12 +0300

    Add Facade for DB Calls

--- src/main/java/com/epam/jmp/client/Main.java
+++ src/main/java/com/epam/jmp/client/Main.java
@@ -3,6 +3,8 @@ package com.epam.jmp.client;
 
 import com.epam.jmp.api.PersonReader;
 import com.epam.jmp.api.ResourceException;
+import com.epam.jmp.api.PersonFacade;
+import com.epam.jmp.api.db.DBPersonFacade;
 import com.epam.jmp.api.PersonManager;
 import com.epam.jmp.api.PersonWriter;
 import com.epam.jmp.api.Person;
@@ -10,34 +12,35 @@ import com.epam.jmp.api.Person;
 
 public class Main {
 
-    private static final String DB_URL = "jdbc:h2:./test;INIT=RUNSCRIPT FROM 'classpath:create.sql'";
-    private static final String DB_LOGIN = "user";
-    private static final String DB_PASSWORD = "";
 
     private static final String FILE_NAME = "persons.txt";
 
     public static void main(String[] args) throws ResourceException{
 
-        PersonManager personManager;
-
-        if(args.length == 1 && "db".equals(args[0])) {
-            personManager = PersonManager.from(DB_URL, DB_LOGIN, DB_PASSWORD);
-        } else {
-            //Use file by default
-            personManager = PersonManager.from(FILE_NAME);
-        }
-
         Person mentee = new Person("Stanislau", "Kaladziuk");
         Person mentor = new Person("Viktar", "Strok");
 
-        PersonWriter personWriter = personManager.getWriter();
-        PersonReader personReader = personManager.getReader();
+        if(args.length == 1 && "db".equals(args[0])) {
+            PersonFacade personFacade = new DBPersonFacade();
 
-        personWriter.writeObject(mentee);
-        personWriter.writeObject(mentor);
+            personFacade.writeObject(mentee);
+            personFacade.writeObject(mentor);
 
-        mentee = personReader.readPerson("Stanislau");
-        mentor = personReader.readPerson();
+            mentee = personFacade.readPerson("Stanislau");
+            mentor = personFacade.readPerson();
+        } else {
+            //Use file by default
+            PersonManager personManager = PersonManager.from(FILE_NAME);
+
+            PersonWriter personWriter = personManager.getWriter();
+            PersonReader personReader = personManager.getReader();
+
+            personWriter.writeObject(mentee);
+            personWriter.writeObject(mentor);
+
+            mentee = personReader.readPerson("Stanislau");
+            mentor = personReader.readPerson();
+        }
 
         System.out.println(mentee);
         System.out.println(mentor);
